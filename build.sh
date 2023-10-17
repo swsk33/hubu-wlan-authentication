b65f3f5424b935edfea55e92ca017fbd5f7f53d4
守望时空33 <yanhanhui2001a@163.com>
2023-10-17 14:26:14 +0800

    完成构建脚本

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ if [ -z "$version" ]; then
 	exit
 fi
 
+basename=hubu-wlan
+
 # 创建文件夹并清理
 mkdir -p ./target
 echo 清理上次构建...
@@ -16,14 +18,31 @@ rm -rf ./target/*
 cd ./src
 echo 开始构建资源...
 go-winres make --in "../winres/winres.json"
-echo 开始构建程序...
-GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o "../target/hubu-wlan.exe"
+
+# 构建Windows程序
+mkdir -p ../target/win/i386
+mkdir -p ../target/win/amd64
+echo 正在构建Windows i386
+GOOS=windows GOARCH=386 go build -ldflags "-w -s" -o "../target/win/i386/${basename}.exe"
+echo 正在构建Windows amd64
+GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o "../target/win/amd64/${basename}.exe"
 rm ./*.syso
+
+# 构建Linux程序
+mkdir -p ../target/linux/i386
+mkdir -p ../target/linux/amd64
+echo 正在构建Linux i386
+GOOS=linux GOARCH=386 go build -ldflags "-w -s" -o "../target/linux/i386/${basename}"
+echo 正在构建Linux amd64
+GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o "../target/linux/amd64/${basename}"
 
 # 压缩可执行文件
 cd ../target
 echo 开始压缩可执行文件...
-upx -9 ./hubu-wlan.exe
+upx -9 ./win/i386/${basename}.exe
+upx -9 ./win/amd64/${basename}.exe
+upx -9 ./linux/i386/${basename}
+upx -9 ./linux/amd64/${basename}
 
 # 创建配置文件
 echo 创建默认配置...
@@ -37,6 +56,10 @@ echo "# 初次启动延迟（秒）" >>./config.yaml
 echo "delay: 3" >>./config.yaml
 
 # 打包
+mkdir -p ./out
 echo 进行打包...
-7z a -t7z -mx9 "./hubu-wlan-${version}.7z" ./hubu-wlan.exe ./config.yaml ../batch/*.bat
-echo 构建完成！构建压缩包位于：target/hubu-wlan-${version}.7z
+7z a -t7z -mx9 "./out/${basename}-${version}-windows-i386.7z" ./win/i386/${basename}.exe ./config.yaml ../batch/*.bat
+7z a -t7z -mx9 "./out/${basename}-${version}-windows-amd64.7z" ./win/amd64/${basename}.exe ./config.yaml ../batch/*.bat
+tar -cJvf "./out/${basename}-${version}-linux-i386.tar.xz" --transform "s=linux/i386/==" ./linux/i386/${basename} ./config.yaml
+tar -cJvf "./out/${basename}-${version}-linux-amd64.tar.xz" --transform "s=linux/amd64/==" ./linux/amd64/${basename} ./config.yaml
+echo 构建完成！结果位于./target/out目录下
